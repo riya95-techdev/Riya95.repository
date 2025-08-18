3a569c5ab7d4b44b0755f68cf854fc290d5f252a
riya95-techdev <nriya0804@gmail.com>
2025-08-18 17:04:15 +0530

    Today's Practice

--- src/PracticeSession.java
+++ src/PracticeSession.java
@@ -1,24 +1,165 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PracticeSession {
     public static void main(String [] args){
+        Scanner riya = new Scanner(System.in);
 //        int userInput = 1;
-//        switch(userInput){
-//            case 1-> System.out.println("book adding in progress....");
-//            case 2-> System.out.println("printing all books....");
-//            case 3-> System.out.print("book is about to update....");
-//            case 4-> System.out.print("book is about to deleted");
-//            case 5-> System.out.print("thanks for using the program!");
+//        switch(userInput) {
+//            case 1 -> System.out.println("book adding in progress....");
+//            case 2 -> System.out.println("printing all books....");
+//            case 3 -> System.out.print("book is about to update....");
+//            case 4 -> System.out.print("book is about to deleted");
+//            case 5 -> System.out.print("thanks for using the program!");
 //            default -> System.out.println("Invalid input ! try again");
+//        }
 
-        List<String> books = new ArrayList<>();
-        books.add("the hbbit");
-        books.add("punctuation");
-        System.out.println(books);
-        System.out.println(books.remove(0));
-        System.out.println(books);
-        System.out.println(books.size());
-        System.out.println(books.contains("the hbbit"));
-        System.out.println(books.isEmpty());
+//        List<String> books = new ArrayList<>();
+//        books.add("the hbbit");
+//        books.add("punctuation");
+//        System.out.println(books);
+//        System.out.println(books.remove(0));
+//        System.out.println(books);
+//        System.out.println(books.size());
+//        System.out.println(books.contains("the hbbit"));
+//        System.out.println(books.isEmpty());
+
+//        int num1, num2,result;
+//        num1=15;
+//        num2=10;
+//        result = num1-num2;
+//        System.out.print("Difference between num1(15) and num2(10) is : "+ result);
+
+//        String cityName;
+//        System.out.println("Hey! which is your favourite city : ");
+//        cityName= riya.nextLine();
+//        System.out.println("Wow, "+ cityName +" is a great and wonderful city.");
+
+//        int enteredNumber;
+//        System.out.println("please enter a number to checking it's postivity : ");
+//        enteredNumber = riya.nextInt();
+//        if(enteredNumber>0)
+//            System.out.println("The entered number is positive.");
+//        else
+//            System.out.println("The entered number is not positive.");
+
+//        // List creation and printing
+//        List<String> shoppingList = new ArrayList<>();
+//        shoppingList.add("Milk");
+//        shoppingList.add("Bread");
+//        shoppingList.add("Eggs");
+//        System.out.println("Shopping List = " +shoppingList);
+
+
+//        String message= """
+//                enter 1 for print Sunday
+//                enter 2 for print Monday
+//                enter 3 for print Tuesday
+//                enter 4 for print Wednesday
+//                enter 5 for print Thursday
+//                enter 6 for print Friday
+//                enter 7 for print Saturday
+//                enter 8 for exit from the code
+//                """;
+//        int dayNumber;
+//
+//        do{
+//            System.out.println(message);
+//            dayNumber =riya.nextInt();
+//            switch(dayNumber){
+//                case 1->
+//                    System.out.println("Today is funday, because of Sunday.");
+//                case 2->
+//                    System.out.println("Something is special today, because of Monday.");
+//                case 3->
+//                    System.out.println("The day of mangal bhawan, its tuesday");
+//                case 4->
+//                    System.out.print("The day of intelligenc owner, its Wednesday.");
+//                case 5->
+//                    System.out.println("The day of peace, its Thursday");
+//                case 6->
+//                    System.out.println("The day of maa santoshi maa bhagwati, it's Friday today.");
+//                case 7->
+//                    System.out.println("The day of Shanidev, its Saturday.");
+//                case 8->
+//                        System.out.println("Exited Successfully.");
+//
+//                default->
+//                    System.out.println("Invalid Number! Please try again");
+//            }
+//
+//        }while(dayNumber<=7);
+
+
+//        int sizeOfCharacterList;
+//        List<Character> characterList= new ArrayList<>();
+//
+//        characterList.add('a');
+//        characterList.add('b');
+//        characterList.add('c');
+//        System.out.println(characterList);
+//        sizeOfCharacterList = characterList.size();
+//        System.out.println("Size of character List : "+ sizeOfCharacterList);
+
+
+//        int number ;
+//        System.out.print("Please enter a number for checking divisibility with 5 : ");
+//        number = riya.nextInt();
+//        if(number%5 ==0)
+//            System.out.println("Number is completely divisible with 5.");
+//        else
+//            System.out.println("Number is not completely divisible by 5.");
+
+
+//        String favoriteFood ;
+//        int i=1;
+//        System.out.println("aapke 3 favorite foods ke name batayiye");
+//        List<String> listOfFavoriteFood = new ArrayList<>();
+//        do{
+//            System.out.print(i+". favorite food : ");
+//            favoriteFood=riya.nextLine();
+//            listOfFavoriteFood.add(favoriteFood);
+//            i++;
+//        }while(i<=3);
+//        System.out.println("List of favorite food : "+listOfFavoriteFood);
+
+//        double num1 ,num2,result;
+//        System.out.println("enter two numbers for performing calculation : ");
+//        num1 = riya.nextDouble();
+//        num2 = riya.nextDouble();
+//        String message= """
+//                       enter '+' for performing addition
+//                       enter '-' for performing subtraction
+//                       enter '*' for performing multiplication
+//                       enter '/' for performing division
+//                       """;
+//        char operator;
+//        System.out.println(message);
+//        operator = riya.next().charAt(0);
+//        switch(operator){
+//            case '+'->{
+//                        result=num1+num2;
+//                        System.out.println("Addition of two entered numbers : "+result);
+//            }
+//            case '-'->{
+//                    result=num1-num2;
+//                    System.out.println("Subtraction of two entered numbers : "+result);
+//            }
+//            case '*'->{
+//                    result=num1*num2;
+//                    System.out.println("Multiplication of two entered numbers : "+result);
+//            }
+//            case'/'->{
+//                    result=num1/num2;
+//                    System.out.println("Division of two entered numbers : "+result);
+//            }
+//            default ->
+//                    System.out.println("invalid input please try again.");
+//        }
+
+
+        
+
     }
 }
