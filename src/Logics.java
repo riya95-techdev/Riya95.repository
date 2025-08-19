49c6d925ba1ee53db5052bc259c508f0d8a291c4
riya95-techdev <nriya0804@gmail.com>
2025-08-19 23:42:17 +0530

    Holds basic logic of method and it's use

--- src/Logics.java
+++ src/Logics.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
-public class Logics {
+public class Logics{
+    //A method that takes two integers and returns their sum
+    public int add(int num1,int num2)
+    {
+        int sum = num1+num2;
+        return sum;
+    }
+    public static void main(String[]args)
+    {
+        Logics myCalculation = new Logics();
+        int result = myCalculation.add(3,6); //Calling the method with arguments
+        System.out.println("The sum is : "+ result); //Output: The sum is : 9
+    }
 }
