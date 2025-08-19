4a96be1f8b2be61970d4b4cd94fe2d22b4e7d3d2
riya95-techdev <nriya0804@gmail.com>
2025-08-20 02:06:35 +0800

    Practiced of basics of methods

--- src/MethodsPractice.java
+++ src/MethodsPractice.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class MethodsPractice {
+
+    public static int addNumbers(int a, int b)
+    {
+        return a+b;
+    }
+    // A method that doesn't return anything
+    public void sayHello()
+    {
+        System.out.println("Hello, Welcome to java methods.");
+    }
+
+    public static void main (String[]args){
+
+        //We need to create an object of the class to call a non-static method
+        MethodsPractice myGreeting = new MethodsPractice();
+        myGreeting.sayHello(); //calling the method
+    }
 }
